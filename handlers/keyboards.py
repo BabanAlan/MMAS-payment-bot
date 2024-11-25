ed736ef19819d3597145391c02a76b1176435fd8
BabanAlan <99186635+BabanAlan@users.noreply.github.com>
2024-11-25 05:13:44 +0300

    Update keyboards.py
    инлайн-клавиатура выбора студии

--- handlers/keyboards.py
+++ handlers/keyboards.py
@@ -1,5 +1,8 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from handlers.buttons import buttons
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton 
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from handlers.buttons import buttons, studios
 
 def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
     row = [KeyboardButton(text=item) for item in items]
@@ -14,3 +17,26 @@ def base_keyboard():
 def payment_keyboard():
     keyboard = make_row_keyboard([buttons['back']])
     return keyboard
+
+
+def studios_keyboard():
+    builder = InlineKeyboardBuilder()
+    # Создание кнопок со студиями
+    for i in range(len(studios)):
+        builder.row(
+            InlineKeyboardButton(
+                text=studios[i],
+                callback_data=f"sdo_{studios[i]}"
+            )
+        )
+
+    # Создание кнопки "назад"
+    builder.row(
+        InlineKeyboardButton(
+            text=buttons['back'],
+            callback_data=buttons['back']
+        )
+    )
+
+    builder.adjust(2)
+    return builder.as_markup()
