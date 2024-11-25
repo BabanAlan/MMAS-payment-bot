e05b5b29e2ecda21a5ff20d63290a2fa8e0c3527
BabanAlan <99186635+BabanAlan@users.noreply.github.com>
2024-11-25 05:12:41 +0300

    Update common.py
    обработка отмены по инлайн-кнопке

--- handlers/common.py
+++ handlers/common.py
@@ -3,11 +3,12 @@ from aiogram.filters import Command
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, CallbackQuery
 
-from keyboards import *
+from handlers.keyboards import *
 from handlers.phrases import common_phrases
 from handlers.buttons import buttons
+from bot import bot
 
 router = Router()
 
@@ -22,10 +23,21 @@ async def cmd_start(message: Message, state: FSMContext):
 
 
 # Обработка кнопки отмены операции
-@router.message(StateFilter(F.text == buttons['back'])
+@router.message(F.text == buttons['back'])
 async def cmd_cancel_no_state(message: Message, state: FSMContext):
     await state.set_data({})
     await message.answer(
+        text=common_phrases['cancel'],
+        reply_markup=base_keyboard()
+    )
+
+
+# Обработка инлайн-кнопки отмены операции 
+@router.callback_query(F.data == buttons['back'])
+async def call_cancel(call: CallbackQuery, state: FSMContext):
+    await state.set_data({})
+    await bot.send_message(
+        chat_id=call.message.chat.id,
         text=common_phrases['cancel'],
         reply_markup=base_keyboard()
     )
