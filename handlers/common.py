239bd3980430780ed61727f4e593e570f175c286
BabanAlan <99186635+BabanAlan@users.noreply.github.com>
2024-11-25 05:06:29 +0300

    Update common.py
    стартовый текст через словарь common_phrases
    убрал стейты в обработке отмены операции

--- handlers/common.py
+++ handlers/common.py
@@ -16,18 +16,13 @@ router = Router()
 async def cmd_start(message: Message, state: FSMContext):
     await state.clear()
     await message.answer(
-        text="Сюда вы можете присылать чеки, для подтверждения оплаты тренировки"
-        "Инструкция по получению чека - https://intercom.help/selector/ru/articles/8355060-"
-        "где-найти-и-как-должен-выглядеть-чек-об-оплате",
-
+        text=common_phrases['start'],
         reply_markup=base_keyboard()
     )
 
 
-# Всевозможная обработка сообщения об отмене операции
-@router.message(StateFilter("PaymentSending:sending_photo"), F.text == buttons['back'])
-@router.message(StateFilter("PaymentSending:sending_name"), F.text == buttons['back'])
-@router.message(StateFilter("PaymentSending:sending_date"), F.text == buttons['back'])
+# Обработка кнопки отмены операции
+@router.message(StateFilter(F.text == buttons['back'])
 async def cmd_cancel_no_state(message: Message, state: FSMContext):
     await state.set_data({})
     await message.answer(
