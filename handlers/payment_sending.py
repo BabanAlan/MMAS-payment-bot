87f37d6784e6b86033d9bef84e1bc3a29e3056cf
BabanAlan <99186635+BabanAlan@users.noreply.github.com>
2025-01-28 19:00:06 +0300

    Update payment_sending.py

--- handlers/payment_sending.py
+++ handlers/payment_sending.py
@@ -1,28 +1,28 @@
-from aiogram import Router, F
+from aiogram import Bot, Router, F
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, CallbackQuery
 from aiogram.methods import forward_message
-from aiogram import Bot, Dispatcher
 
-from bot import bot
 from config import config
 from handlers.keyboards import *
 from handlers.phrases import payment_phrases
 from handlers.buttons import buttons
 
 router = Router()
+bot = Bot(config['bot_token'])
 
 
 # Состояния диалога в сценарии отправки чека
 class PaymentSending(StatesGroup):
     sending_photo = State()
     sending_name = State()
-    sending_date = State()
+    sending_type = State()
+    sending_duration = State()
 
 
-# Инициация сценария отправки чека
+# Инициация сценария отправки чека, отправка чека
 @router.message(F.text == buttons['add_payment'])
 async def cmd_payment(message: Message, state: FSMContext):
     await message.answer(
@@ -32,7 +32,7 @@ async def cmd_payment(message: Message, state: FSMContext):
     await state.set_state(PaymentSending.sending_photo)
 
 
-# Получили фото
+# Получили фото, отправка ФИО
 @router.message(PaymentSending.sending_photo, F.photo != None)
 async def photo_sent(message: Message, state: FSMContext):
     await state.update_data(sent_photo=message)
@@ -52,15 +52,15 @@ async def photo_sent_incorrectly(message: Message):
     )
 
 
-# Получили имя
+# Получили ФИО, отправка типа оплаты
 @router.message(PaymentSending.sending_name, F.text != None)
 async def name_sent(message: Message, state: FSMContext):
     await state.update_data(sent_name=message.text)
     await message.answer(
-        text=payment_phrases['payment_date'],
-        reply_markup=payment_keyboard()
+        text=payment_phrases['payment_type'],
+        reply_markup=types_keyboard()
     )
-    await state.set_state(PaymentSending.sending_date)
+    await state.set_state(PaymentSending.sending_type)
 
 
 # Имя не было получено
@@ -71,11 +71,33 @@ async def name_sent_incorrectly(message: Message):
         reply_markup=payment_keyboard()
     )
 
+# Получили тип оплаты, отправка продолжительности оплаты
+@router.callback_query(PaymentSending.sending_type, F.data.startswith('tps_'))
+async def type_sent(call: CallbackQuery, state: FSMContext):
+    await state.update_data(sent_type=call.data.replace('tps_', ''))
+    await bot.send_message(
+        chat_id=call.message.chat.id,
+        text=payment_phrases[f'paymrnt_duration_{call.data[4]}'],
+        reply_markup=base_keyboard()
+    )
+    await state.set_state(PaymentSending.sending_duration)
 
-# Получили дату
-@router.message(PaymentSending.sending_date, F.text != None)
-async def name_sent(message: Message, state: FSMContext):
+
+# Тип оплаты не был получен
+@router.message(PaymentSending.sending_type)
+async def name_sent_incorrectly(message: Message):
+    await message.answer(
+        text=payment_phrases['payment_type'],
+        reply_markup=types_keyboard()
+    )
+
+
+# Продолжительность оплаты получена
+@router.message(PaymentSending.sending_duration, F.text != None)
+async def duration_sent(message: Message, state: FSMContext):
+    await state.update_data(sent_duration=message.text)
     user_data = await state.get_data()
+
     # Пересылка сообщения с картинкой(чтобы был виден отправитель)
     await bot.forward_message(
         chat_id=config['admins_chat_id'],
@@ -85,21 +107,23 @@ async def name_sent(message: Message, state: FSMContext):
     # Сборка остальной информации в одно сообщение и отправка
     await bot.send_message(
         chat_id=config['admins_chat_id'],
-        text=f"{user_data['sent_name']}\n{message.text}"
+        # callback_data студии - это sto_ + название студии - Избавляемся от головы
+        text=f"{user_data['sent_name']}\n{user_data['sent_type'][1:]}\n{user_data['sent_duration']}"
     )
     # Отправка завершающего сценарий сообщения
-    await message.answer(
+    await bot.send_message(
+        chat_id=message.chat.id,
         text=payment_phrases['payment_finish'],
         reply_markup=base_keyboard()
     )
     # Сброс state
     await state.clear()
 
-
-# Дата не была получена
-@router.message(PaymentSending.sending_date)
-async def name_sent_incorrectly(message: Message):
+# Продолжительность оплаты не получена
+@router.message(PaymentSending.sending_duration)
+async def duration_sent_incorrectly(message: Message):
     await message.answer(
-        text=payment_phrases['payment_date_incorrect'],
+        text=payment_phrases['payment_img_incorrect'],
         reply_markup=payment_keyboard()
     )
+
